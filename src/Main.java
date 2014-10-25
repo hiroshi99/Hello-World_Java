703b9eb4e4ba5bd8d90d869ce49d5a5ccb742ab3
Hiroshi Yokkaichi <teslin99@nifty.com>
2014-10-25 17:27:37 +1300

    Change Message 17:27

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hellow World Java");
+			System.out.println("Have a pleasant evening");
 	}
 
 }
